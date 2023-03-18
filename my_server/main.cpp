d3c9814fd688b90e6cc8ce62b026f6cc0e6f44e6
阿文 <1822485056@qq.com>
2023-03-19 03:16:09 +0800

    实现资源路径解析

--- my_server/main.cpp
+++ my_server/main.cpp
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <WinSock2.h>
+#include <string.h>
 #pragma comment(lib,"WS2_32.lib")
+
+#define PRINTF(str) printf("[%s - %d]"#str "=%s\n", __func__, __LINE__, str);
 
 void error_die(const char* str) {
 	perror(str);
 	exit(1);
 }
 
-int startup(unsigned short *port) {
+int startup(unsigned short* port) {
 	//1.网络通信初始化
 	WSADATA data;
 	int ret = WSAStartup(
@@ -23,7 +26,7 @@ int startup(unsigned short *port) {
 		IPPROTO_TCP);   //使用tcp通信协议
 	if (server_socket == -1) {
 		error_die("套接字创建失败");
-		}
+	}
 
 	//3.设置端口可复用
 	int opt = 1;
@@ -35,11 +38,11 @@ int startup(unsigned short *port) {
 
 	//4.配置服务器的网络地址
 	struct sockaddr_in server_addr;
-	memset(&server_addr,0, sizeof(server_addr));
+	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(*port);
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	
+
 	//5.绑定套接字
 	if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
 		error_die("套接字绑定失败");
@@ -61,30 +64,102 @@ int startup(unsigned short *port) {
 	return server_socket;
 }
 
+//从指定套接字中读取一行数据,保存到buff中，返回实际读取到的字节数
+int get_line(int sock, char* buff, int size) {
+	char c = 0; //'\0'
+	int i = 0;
+	while (i < size - 1 && c != '\n') {
+		int n = recv(sock, &c, 1, 0);
+		if (n > 0) {
+			if (c == '\r') {
+				n = recv(sock, &c, 1, MSG_PEEK);
+				if (n > 0 && c == '\n') {
+					recv(sock, &c, 1, 0);
+				}
+				else {
+					c = '\n';
+				}
+			}
+			buff[i++] = c;
+		}
+		else {
+			// 
+			c = '\n';
+		}
+	}
+	buff[i] = 0; //'\0'
+	return i;
+}
+
+void unimplement(int client) {
+	//向指定套接字发送一个提示还没有实现的错误页面
+}
+
 //处理用户请求的服务线程
 DWORD WINAPI accept_request(LPVOID arg) {
+
+
+	char buff[1024];
+	int client = (SOCKET)arg; //客户端套接字
+
+	//读取一行数据
+	int numchars = get_line(client, buff, sizeof(buff));
+	PRINTF(buff);
+
+	char method[255];
+	int j = 0, i = 0;
+	while (!isspace(buff[j]) && i < sizeof(method) - 1) { //isspace为跳过空白字符
+		method[i++] = buff[j++];
+	}
+	method[i] = 0;
+	PRINTF(method);
+
+	//检查请求的方法,本服务器是否支持
+	if (stricmp(method, "GET") && stricmp(method, "POST")) {
+		//像浏览器返回一个错误提示页面
+		unimplement(client);
+		return 0;
+	}
+
+	//解析资源文件的路径
+	char url[255]; //存放请求资源的完成路径
+	i = 0;
+	//跳过资源路径前边的空格
+	while (isspace(buff[j]) && j < sizeof(buff)) j++; //isspace为跳过空白字符
+
+	while (!isspace(buff[j]) && i < sizeof(url) - 1 && j < sizeof(buff)) {
+		url[i++] = buff[j++];
+	}
+	url[i] = 0;
+	PRINTF(url);
+
+
 	return 0;
 }
 
 int main() {
 
-	unsigned short port = 0;
+	unsigned short port = 80;
 	int server_sock = startup(&port);
 	printf("服务已启动，正在监听 %d 端口...", port);
-	
+
 	struct sockaddr_in client_addr;
 	int client_addr_len = sizeof(client_addr);
 	while (1) {
 		//阻塞式等待用户通过浏览器访问
-		int client_sock = accept(server_sock,(struct sockaddr*)&client_addr, &client_addr_len);
+		int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_len);
 		if (client_sock == -1) {
 			error_die("链接失败");
 		}
 		//创建一个新的线程
 		DWORD threadId = 0;
-		CreateThread(0, 0, accept_request,(void*)client_sock,0,&threadId);
+		CreateThread(0, 0, accept_request, (void*)client_sock, 0, &threadId);
 
 	}
+
+	// "/" 网站服务器资源目录下的 index.html
+
+
 	closesocket(server_sock);
 	return 0;
 }
